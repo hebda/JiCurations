33915887bef4cc0ea3e5081b8f6011f5f451439d
hebda <phil.hebda@gmail.com>
2017-06-24 12:03:04 -0700

    subscription list

--- app/views.py
+++ app/views.py
@@ -7,21 +7,37 @@ import matplotlib.pyplot as plt
 import math
 import os
 import collections
+from datetime import datetime
 
 font = {'family' : 'sans-serif', 'weight' : 'bold', 'size'   : 22}
 plt.rc('font', **font)
 #plt.rcParams['text.latex.preamble'] = [r'\usepackage{sfmath} \boldmath']
 
+# for determining if email input box is needed
+is_subscribed=0
+
 @app.route('/')
 @app.route('/index')
 @app.route('/input')
-def git_input():
-
-    return render_template("index.html")
+def home():
+    return render_template("index.html",is_subscribed=is_subscribed)
 
 @app.route('/coming_soon')
-def slides():
-    return render_template('coming_soon.html')
+def coming_soon():
+    return render_template('coming_soon.html',is_subscribed=is_subscribed)
+
+@app.route('/subscribe')
+def subscribe():
+    email_input=request.args.get('email_input')
+    dateint=int(datetime.utcnow().strftime('%Y%m%d'))
+    if '@' in email_input and '.' in email_input:
+        is_subscribed=1
+        db = mdb.connect(user="root", host="localhost", db="JiCurations", charset='utf8')
+        with db:
+            cur = db.cursor()
+            cur.execute("INSERT INTO subscription (email, subscription_date) values (\"%s\",%d);" % (email_input,dateint) )
+
+    return render_template('index.html',is_subscribed=is_subscribed)
 
 @app.route('/error')
 def error():
